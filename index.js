219b0d80d2ebbdce12619ef4dccce0af290d8db3
Max <tutv95@gmail.com>
2018-10-24 18:18:27 +0700

    add express

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log('Hello world')
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => {
+	res.send('hello world')
+})
+
+app.get('/get-something', (req, res) => {
+	res.send({
+		hello: 'world',
+	})
+})
+
+app.listen(3000, () => {
+	console.log('hello')
+})
